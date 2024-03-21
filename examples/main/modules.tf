6ce52c650067f8ab267a239c67e805910282b5b0
Ankit Kaushik <klatte09>
2024-03-21 10:25:34 +0530

    merge conflict resolved

--- examples/main/modules.tf
+++ examples/main/modules.tf
@@ -1,23 +1,5 @@
-module "azure_region" {
-  source  = "claranet/regions/azurerm"
-  version = "x.x.x"
-
-  azure_region = var.azure_region
-}
-
-module "rg" {
-  source  = "claranet/rg/azurerm"
-  version = "x.x.x"
-
-  location    = module.azure_region.location
-  client_name = var.client_name
-  environment = var.environment
-  stack       = var.stack
-}
-
 module "logs" {
-  source  = "claranet/run/azurerm//modules/logs"
-  version = "x.x.x"
+  source  = "git::https://github.com/tothenew/terraform-azure-loganalytics.git"
 
   client_name         = var.client_name
   environment         = var.environment
@@ -27,25 +9,8 @@ module "logs" {
   resource_group_name = module.rg.resource_group_name
 }
 
-module "lb" {
-  source  = "claranet/lb/azurerm"
-  version = "x.x.x"
-
-  client_name    = var.client_name
-  environment    = var.environment
-  location       = module.azure_region.location
-  location_short = module.azure_region.location_short
-  stack          = var.stack
-
-  resource_group_name = module.rg.resource_group_name
-
-  allocate_public_ip = true
-  enable_nat         = true
-}
-
 module "eventhub" {
-  source  = "claranet/eventhub/azurerm"
-  version = "x.x.x"
+  source  = "git::https://github.com/tothenew/terraform-azure-eventhub.git"
 
   location       = module.azure_region.location
   location_short = module.azure_region.location_short
@@ -73,8 +38,7 @@ module "eventhub" {
 }
 
 module "diagnostic_settings" {
-  source  = "claranet/diagnostic-settings/azurerm"
-  version = "x.x.x"
+  source  = "git::https://github.com/tothenew/terraform-azure-diagnostics.git"
 
   resource_id = module.lb.lb_id
 
